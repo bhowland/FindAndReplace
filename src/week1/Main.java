0be409b71057cdae6b97b66f31d7cdd796bb58b7
bhowland <bhowland@madisoncollege.edu>
2015-10-12 15:22:15 -0500

    added a example of logging

--- src/week1/Main.java
+++ src/week1/Main.java
@@ -1,9 +1,16 @@
 package week1;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
 /**
  * Created by ben on 9/18/2015.
  */
+
+@WebService
 public class Main {
+
+    @WebMethod
     public static void main(String[] args) {
         ReadTheDocs newLetter = new ReadTheDocs("findandreplace.txt", "input.txt", "readyToSend.txt");
         newLetter.readInValues();
